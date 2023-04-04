b345011f3a49532d9781bb40fb3d0f0ece78657c
Laureano <laau262@gmail.com>
2023-04-04 19:31:26 -0300

    Ejercicio 01 Resuelto

---
+++ ejercicio_01/src/ejercicio_01/Principal.java
@@ -0,0 +1,25 @@
+package ejercicio_01;
+
+import java.util.Iterator;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Mostrar por consola los 10 primeros números enteros y los 10 primeros números impares
+		
+		for (int i = 0; i < 11; i++) {
+			
+			System.out.print(i);
+			if( !(i % 2 == 0)) {
+				System.out.print(" Es Impar");
+			}
+			System.out.println("\n");
+			
+		}
+		
+		
+		
+	}
+
+}
---
+++ ejercicio_01/src/module-info.java
@@ -0,0 +1,2 @@
+module ejercicio_01 {
+}
