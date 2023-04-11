a91f20a7eba2312c646eab80f6a9c420f79d55eb
Laureano <laau262@gmail.com>
2023-04-11 00:40:28 -0300

    Este ejercicio tiene error con el Scanner, se plantea la posible solución

--- ejercicio_05a/src/ejercicio_05a/Principal_05.java
+++ ejercicio_05a/src/ejercicio_05a/Principal_05.java
@@ -9,6 +9,12 @@ public class Principal_05 {
   *vendedores y cargar todos los datos respectivos.
   *Luego listar los empleados indicando: dni, nombre, apellido y sueldo.
 */	
+		
+		// Contiene errores por culpa del Scanner
+		// Una posible solución es instanciarla aquí y pasarlo como un parametro adicional
+		// Por lo que habría que modificar los demas metodos
+		// Una vez finalizado todos los inputs lo cerramos desde aquí
+		
 		Empresa e = new Empresa();
 		Empleado emp = new Empleado();
 		Vendedor v = new Vendedor();
